3e1dcbeaec743318e374701ecc56a88e62703880
lwx <1172041421@qq.com>
2024-04-20 14:39:02 +0800

    message 8: Before refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -28,13 +28,18 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1:Buy flowers', [row.text for row in rows])
+        self.assertIn('1: Buy flowers', [row.text for row in rows])
+        self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
 
         self.fail("Finish the test!")
 
